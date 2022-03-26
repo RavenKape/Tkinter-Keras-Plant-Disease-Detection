180e47b5fc29ab4e8b3ad87bf2c5f707caefa08c
LawrenceAmar <80806278+LawrenceAmar@users.noreply.github.com>
2022-03-26 17:30:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -71,9 +71,24 @@ def showimage():
     npp_image = np.expand_dims(np_image_li, axis=0)
     result = model.predict(npp_image)
     finalresult = Categories[np.argmax(result[0])]
-    font_size = int(((wt/2.41)-(ht/2))/7)
-    canvas1.create_rectangle((wt/2.75)+50, ht/2.25, (wt/1.50)+50, (ht/1.5)+15, outline = "black", fill = "white", tags = "result_box")
-    canvas1.create_text((wt/2)+75, (ht/1.75)-40, text = "Result:\n"+ finalresult, font = "Times " + str(font_size) + " bold", tags = "result")
+    font_size = int(((wt/2.41)-(ht/2))/10)
+    canvas1.create_rectangle((wt/5.65)+(wt/4.7), ht/2.25, (wt/2.78)+(wt/3), (ht/2.78)+(ht/2.2), outline = "black", fill = "white", tags = "result_box")
+    canvas1.create_text((wt/2), (ht/2), text = "Result:\n"+ finalresult, font = "Times " + str(font_size) + " bold", tags = "result")
+
+    management = ""
+    if Categories[np.argmax(result[0])] == "Potato___Early_blight":
+        text_wt = (wt/3.5)+(wt/4)
+        text_ht = (ht/1.5)
+        management = "Remove the crop debris \nand disinfect tools after harvest. \nThen, purchase disease-free \nseed and wash them in a bleach \ntreatment. Crop rotation \nis also recommended."
+    elif Categories[np.argmax(result[0])] == "Pepper_bell___healthy":
+        text_wt = (wt/4.48)+(wt/3.75)
+        text_ht = (ht/1.7)
+        management = "The plant is healthy"
+    elif Categories[np.argmax(result[0])] == "Potato___Early_bliht":
+        text_wt = (wt/4.48)+(wt/3.75)
+        text_ht = (ht/1.7)
+        management = "The plant is healthy"    
+    canvas1.create_text(text_wt, text_ht, text = "Management:\n"+ management, font = "Times " + str(font_size) + " bold", tags = "result")
 
 def helpPage():
     root.destroy()
